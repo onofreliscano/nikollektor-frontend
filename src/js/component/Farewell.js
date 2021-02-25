e24c72c9680b9c10ab630ea8ca813ed35b0dd538
Mauricio Lizcano <mauriciolizcano25531@gmail.com>
2021-02-25 14:36:19 -0400

    cambios

---
+++ src/js/component/Farewell.js
@@ -0,0 +1,19 @@
+// import { Navbar } from "./navbar";
+// import React, { useContext, useState } from "react";
+
+// const Farewell = () => {
+// 	return (
+// 		<div className="Container">
+// 			<div className="row">
+// 				<div className="col-sm">
+// 					{/* <div className="nikollector-container-home-left"> */}
+// 					<h1>Thanks!. You let us know how you feel </h1>
+// 					{/* </div> */}
+// 				</div>
+// 			</div>
+// 			<div className="row">Catharsis done</div>
+// 		</div>
+// 	);
+// };
+
+// export default Farewell;
